c5ff09c2ce139d7d9fb3df01ba03467f72094549
SankethBK <sankethbk7777@gmail.com>
2021-08-07 23:52:46 +0530

    Added projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
 import Intro from './components/Intro/Intro';
 import About from './components/About/About';
+import Project from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <div style={{ marginTop: 'var(--nav-height)' }}>
         <Intro />
         <About />
+        <Project />
         <Contact />
       </div>
     </>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import './Projects.css';
+import myData from '../data.json';
 
 function Projects() {
   return (
     <div id='projects'>
-      <h3>Projects</h3>
+      <h3 className='projects-header'>Projects</h3>
 
       <p className='projects-intro-text'>These are some of my projects</p>
 
-      <div className='projects grid'></div>
+      <div className='projects-grid'>
+        {myData['projects'].map((project, index) => {
+          return (
+            <div key={index} className='card'>
+              <img src={project.thumbnail} alt={project.name + ' thumbnail'} />
+              <div className='info'>
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+                <div className='project-links'>
+                  {project['demo'] && <a href={project['demo']}>demo</a>}
+
+                  {project['source-code'] && (
+                    <a href={project['source-code']}>source code</a>
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
